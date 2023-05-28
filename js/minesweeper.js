68f6f880ccfe273ae8e22ea259ce0c30bb7640ca
Benjamin Klein <benjamink.lein@hotmail.com>
2023-05-28 16:46:39 +0200

    not much but honest work

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
   });
 
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
